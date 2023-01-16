1ad6be7948430f17c8ce35fba7a2c6d4184ff4ae
Gorilla 0xJungle.com <Gorilla@0xJungle.com>
2023-01-16 17:46:46 -0500

    Changed error output to use stderr

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ fn main() {
     println!("Pretty Args: {args:#?}");
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In the file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
